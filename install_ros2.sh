9d0fae473e502ff5b9a3fbb159e92c4b45a6780e
varofla <45277002+varofla@users.noreply.github.com>
2022-12-22 13:50:19 +0900

    수정 : 패키지 저장소 업데이트 한번만 하도록 함

--- install_ros2.sh
+++ install_ros2.sh
@@ -11,14 +11,13 @@ export LANG=en_US.UTF-8
 echo "\n################################################################"
 echo "ROS2 Repository 추가"
 echo "################################################################"
-sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+sudo apt install -y curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 echo "\n################################################################"
 echo "ROS2 Foxy 설치, 적용"
 echo "################################################################"
-sudo apt update
 sudo apt install -y ros-foxy-desktop
 bash /opt/ros/foxy/setup.bash
 bashrc=$(tail ~/.bashrc)
